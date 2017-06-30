2b08d5a0f5ff7254c70291d0e6be6791b4d29dba
Oksana Repkina <repkinaoxana@gmail.com>
2017-06-30 08:29:56 +0300

    Add files to repository

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_blog_tutorials;port=8889;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
